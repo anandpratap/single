ebb970aba240440017879be5f6ec338f56a9b0fa
Anand Pratap Singh <anandps@f22.me>
2015-03-02 00:19:25 -0500

    Added openmp

--- src/modules.f90
+++ src/modules.f90
@@ -1,10 +1,11 @@
 	module params_global
+		use omp_lib
 		real :: gamma, cfl
 		real :: rhoinf, uinf, pinf, rhoo, po, R, pout
 		
 		data rhoinf, uinf, pinf/1.0, 0.0, 1.0/
 		data rhoo, po, pout/1.0, 1.0, 0.7/
-		data gamma, cfl, R/1.4, 1000.0, 287.0/
+		data gamma, cfl, R/1.4, 100.0, 287.0/
 		
 		integer :: log_data
 		data log_data/10/
